9f603f8184d556bd81700370ca92ac7059d9ba5c
Daniel Celebucki <djc40@duke.edu>
2015-11-02 23:15:59 -0500

    added random, decoder and writer

---
+++ writer.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity writer is
+port (	
+		regnum : IN STD_LOGIC_VECTOR(4 downto 0)
+		
+
+);
+end writer;
+
+architecture Behavioral of writer is
+begin
+
+end architecture;
