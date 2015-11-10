676d1d05917fab55961bed7ff5bf0c6396b95d7f
Daniel Celebucki <djc40@duke.edu>
2015-11-09 21:43:29 -0500

    added files

--- Lookup.vhd
+++ Lookup.vhd
@@ -10,7 +10,7 @@ use Work.tableState.all;
 			input_registerArray2 	: std_LOGIC_VECTOR(49 DOWNTO 0);
 			input_registerArray3 	: std_LOGIC_VECTOR (49 DOWNTO 0);
 			input_registerArray4 	: std_LOGIC_VECTOR(49 DOWNTO 0);
-			input_register			: IN STD_LOGIC_VECTOR(49 DOWNTO 0);
+			input_register			: IN STD_LOGIC_VECTOR(47 DOWNTO 0);
 			output_port				: OUT STD_LOGIC_VECTOR(1 DOWNTO 0);
 			output_valid			: OUT STD_LOGIC;
 			output_registerNumber	: OUT STD_LOGIC_VECTOR(4 DOWNTO 0)
@@ -77,46 +77,47 @@ END COMPONENT;
 
 	COMPONENT onehot_binary
 	PORT ( 
-			onehot_in  : IN  STD_LOGIC_VECTOR(4 DOWNTO 0);
-	       	binary_out : OUT STD_LOGIC_VECTOR (4 DOWNTO 0) 
+			onehot_in  : IN  STD_LOGIC_VECTOR(31 DOWNTO 0);
+	       	binary_out : OUT STD_LOGIC_VECTOR (4 DOWNTO 0);
+			outVal : OUT STD_LOGIC
 		 );
 	END COMPONENT;
 
 
 	SIGNAL tableRegisterOutput : VECTOR_MAC_PORT(NUM_REGISTERS DOWNTO 0);
 
-	SIGNAL tableComparatorOutput : STD_LOGIC_VECTOR(4 DOWNTO 0);
+	SIGNAL tableComparatorOutput : STD_LOGIC_VECTOR(31 DOWNTO 0);
 	SIGNAL onehot_binaryOutput : STD_LOGIC_VECTOR(4 DOWNTO 0);
 
 BEGIN
 
 
 		tableComparator0 : tableComparator PORT MAP (
-			dataa		=>	input_registerArray0(47 DOWNTO 0),
+			dataa		=>	input_registerArray0(49 DOWNTO 2),
 			datab		=>	input_register(47 DOWNTO 0), 
 			aeb			=>	tableComparatorOutput(0)
 			);
 			
 					tableComparator1 : tableComparator PORT MAP (
-			dataa		=>	input_registerArray1(47 DOWNTO 0),
+			dataa		=>	input_registerArray1(49 DOWNTO 2),
 			datab		=>	input_register(47 DOWNTO 0), 
 			aeb			=>	tableComparatorOutput(1)
 			);
 			
 					tableComparator2 : tableComparator PORT MAP (
-			dataa		=>	input_registerArray2(47 DOWNTO 0),
+			dataa		=>	input_registerArray2(49 DOWNTO 2),
 			datab		=>	input_register(47 DOWNTO 0), 
 			aeb			=>	tableComparatorOutput(2)
 			);
 			
 					tableComparator3 : tableComparator PORT MAP (
-			dataa		=>	input_registerArray3(47 DOWNTO 0),
+			dataa		=>	input_registerArray3(49 DOWNTO 2),
 			datab		=>	input_register(47 DOWNTO 0), 
 			aeb			=>	tableComparatorOutput(3)
 			);
 			
 					tableComparator4 : tableComparator PORT MAP (
-			dataa		=>	input_registerArray4(47 DOWNTO 0),
+			dataa		=>	input_registerArray4(49 DOWNTO 2),
 			datab		=>	input_register(47 DOWNTO 0), 
 			aeb			=>	tableComparatorOutput(4)
 			);
@@ -124,17 +125,18 @@ BEGIN
 
 	onehot_binary0 : onehot_binary
 	PORT MAP (
-			onehot_in 	=> tableComparatorOutput(4 DOWNTO 0),
-			binary_out 	=> onehot_binaryOutput (4 DOWNTO 0)
+			onehot_in 	=> tableComparatorOutput(31 DOWNTO 0),
+			binary_out 	=> onehot_binaryOutput (4 DOWNTO 0),
+			outVal => output_valid
 
 		);
 
 	outputPortMux0 	: outputPortMux PORT MAP (
-			data0x		=> input_registerArray0(49 downto 48),
-			data1x		=> input_registerArray1(49 downto 48),
-			data2x		=> input_registerArray2(49 downto 48),
-			data3x		=> input_registerArray3(49 downto 48),
-			data4x		=> input_registerArray4(49 downto 48),
+			data0x		=> input_registerArray0(1 downto 0),
+			data1x		=> input_registerArray1(1 downto 0),
+			data2x		=> input_registerArray2(1 downto 0),
+			data3x		=> input_registerArray3(1 downto 0),
+			data4x		=> input_registerArray4(1 downto 0),
 			data5x		=> "00",
 			data6x		=> "00",
 			data7x		=> "00",
--- onehot_binary.vhd
+++ onehot_binary.vhd
@@ -4,35 +4,51 @@ use ieee.numeric_std.all;
 
 entity onehot_binary is
   port    ( onehot_in  : in  std_logic_vector(31 downto 0);
-            binary_out : out std_logic_vector(4 downto 0)  );
-  begin
-    --assert 2**binary_out'length = onehot_in'length severity failure;
+            binary_out : out std_logic_vector(4 downto 0) ;
+	    outVal : out std_logic );
 
-end;
+
+end onehot_binary;
 
 architecture rtl of onehot_binary is
-
-  function one_hot_to_binary (
-    One_Hot : std_logic_vector ;
-    size    : natural
-  ) return std_logic_vector is
-
-    variable Bin_Vec_Var : std_logic_vector(size-1 downto 0);
-
-  begin
-
-    Bin_Vec_Var := (others => '0');
-
-    for I in One_Hot'range loop
-      if One_Hot(I) = '1' then
-        Bin_Vec_Var := Bin_Vec_Var or std_logic_vector(to_unsigned(I,size));
-      end if;
-    end loop;
-    return Bin_Vec_Var;
-  end function;
-
 begin
-
-  binary_out <= one_hot_to_binary(onehot_in, binary_out'length);
-
-end;
+process(onehot_in)
+begin
+outVal <='1';
+case onehot_in is
+when "00000000000000000000000000000001" => binary_out <= "00000";
+when "00000000000000000000000000000010" => binary_out <= "00001";
+when "00000000000000000000000000000100" => binary_out <= "00010";
+when "00000000000000000000000000001000" => binary_out <= "00011";
+when "00000000000000000000000000010000" => binary_out <= "00100";
+when "00000000000000000000000000100000" => binary_out <= "00101";
+when "00000000000000000000000001000000" => binary_out <= "00110";
+when "00000000000000000000000010000000" => binary_out <= "00111";
+when "00000000000000000000000100000000" => binary_out <= "01000";
+when "00000000000000000000001000000000" => binary_out <= "01001";
+when "00000000000000000000010000000000" => binary_out <= "01010";
+when "00000000000000000000100000000000" => binary_out <= "01011";
+when "00000000000000000001000000000000" => binary_out <= "01100";
+when "00000000000000000010000000000000" => binary_out <= "01101";
+when "00000000000000000100000000000000" => binary_out <= "01110";
+when "00000000000000001000000000000000" => binary_out <= "01111";
+when "00000000000000010000000000000000" => binary_out <= "10000";
+when "00000000000000100000000000000000" => binary_out <= "10001";
+when "00000000000001000000000000000000" => binary_out <= "10010";
+when "00000000000010000000000000000000" => binary_out <= "10011";
+when "00000000000100000000000000000000" => binary_out <= "10100";
+when "00000000001000000000000000000000" => binary_out <= "10101";
+when "00000000010000000000000000000000" => binary_out <= "10110";
+when "00000000100000000000000000000000" => binary_out <= "10111";
+when "00000001000000000000000000000000" => binary_out <= "11000";
+when "00000010000000000000000000000000" => binary_out <= "11001";
+when "00000100000000000000000000000000" => binary_out <= "11010";
+when "00001000000000000000000000000000" => binary_out <= "11011";
+when "00010000000000000000000000000000" => binary_out <= "11100";
+when "00100000000000000000000000000000" => binary_out <= "11101";
+when "01000000000000000000000000000000" => binary_out <= "11110";
+when "10000000000000000000000000000000" => binary_out <= "11111";
+when others =>outVal <= '0';
+end case;
+end process;
+end rtl;
